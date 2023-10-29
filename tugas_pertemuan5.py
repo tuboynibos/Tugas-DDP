3a7fd6a150d484bdb3dd8b45e184ae4d95bb3ca3
tuboynibos <129910724+tuboynibos@users.noreply.github.com>
2023-10-29 23:45:48 +0700

    Add files via upload

---
+++ tugas_pertemuan5.py
@@ -0,0 +1,16 @@
+kendaraan = ["B 3222 SUX", "Mio Jt", 125, "Putih"]
+kendaraan.append("8 Jt")
+kendaraan.append("2 roda")
+kendaraan.insert(2, "Yamaha")
+kendaraan.insert(3, "sepeda motor")
+
+
+
+print ("kode kendaraan:",kendaraan[0])
+print ("Nama Kendaraan:", kendaraan[1])
+print ("Merk Kendaraan:", kendaraan[2])
+print ("Jenis Kendaraan:", kendaraan[3])
+print ("CC Kendaraan:", kendaraan[4])
+print ("Warna Kendaraan:", kendaraan[5])
+print ("Harga Kendaraan:", kendaraan[6])
+print ("Roda Kendaraan:", kendaraan[7])
